38efee410cc820563ba39cbd20c574a90ad8197c
Maxime Perrotin <Maxime.Perrotin@esa.int>
2017-03-28 16:58:55 +0200

    reorganize test tree

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,1 @@
-../buildsupport -glue  -i interfaceview.aadl -c deploymentview.aadl -d dataview.aadl ocarina_components.aadl TASTE_DV_Properties.aadl
+../buildsupport -glue  -i test1/interfaceview.aadl -c test1/deploymentview.aadl -d test1/dataview.aadl ocarina_components.aadl TASTE_DV_Properties.aadl
